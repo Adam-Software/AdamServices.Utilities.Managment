9259b72a31b395d0738be88f2afa0e431342d888
Alexey Nesterov <vertigra.developer@gmail.com>
2025-02-24 08:05:56 +1000

    Clean code

--- src/Managment.Services.Common/CheckingUpdateService.cs
+++ src/Managment.Services.Common/CheckingUpdateService.cs
@@ -26,9 +26,6 @@ namespace Managment.Services.Common
         private readonly string mRepositoryListPath = "jsonRepository";
         private readonly GitHubClient mGitHubClient;
         private readonly JsonRepository mJsonRepository;
-        //const string cUniqueUrlListFileName = "uniqueUpdateUrls.json";
-        //const string cUrlListFileName = "updateUrls.json";
-        //const string cUrlWithNameListFileName = "repositoryUrlWithName.json";
 
         #endregion
 
@@ -180,98 +177,6 @@ namespace Managment.Services.Common
                 }
             }
         }
-
-        /*public async Task CheckAndSaveUpdateListsAsync()
-        {
-            JsonRepository jsonRepository = new(mRepositoryPath);
-            List<ServiceNameWithUrl> serviceNameWithUrl = [];
-
-
-            try
-            {
-                jsonRepository.CreateOrClearRepositoryDirectory();
-
-                var checkUpdateUrl = mAppSettingsOptionsService.CheckUpdateUrl.ConvertGitHubLinkToRaw();
-                await jsonRepository.SaveJsonResponseAsync(cUrlListFileName, checkUpdateUrl);
-
-                List<ServiceUrlModel> rawUpdateUrls = await jsonRepository.ReadJsonFileAsync<List<ServiceUrlModel>>(cUrlListFileName);
-                List<ServiceUrlModel> convertUrls = RawUrlParser(rawUpdateUrls);
-                List<ServiceUrlModel> uniquensUrls = CheckUniquensUrls(convertUrls);
-
-                if (uniquensUrls.Count == 0)
-                {
-                    mLogger.LogWarning("Service addresses were not found");
-                    return;
-                }
-
-                jsonRepository.SerializeAndSaveServiceUrls(uniquensUrls, cUniqueUrlListFileName);
-
-                foreach (ServiceUrlModel url in uniquensUrls)
-                {
-                    var fileName = $"{url.ServiceUrl.ConvertToRepositoryName()}.json";
-                    await jsonRepository.SaveJsonResponseAsync(fileName, url.ServiceUrl);
-                    mLogger.LogInformation("Json for service name in config {name} with url {url} saved with name {fileName}", url.ServiceName, url.ServiceUrl, url.ServiceUrl.ConvertToRepositoryName());
-                    
-                    ServiceNameWithUrl nameWithUrl = new()
-                    {
-                        ServiceInfoJsonUrl = url.ServiceUrl.ConvertRawUrlToGitUrl(),
-                        ServiceInfoServiceName = url.ServiceName
-                    };
-
-                    serviceNameWithUrl.Add(nameWithUrl);
-                    jsonRepository.SerializeAndSaveServiceNameWithUrls(serviceNameWithUrl, cUrlWithNameListFileName);
-                }
-            }
-            catch (Exception ex) 
-            {
-                mLogger.LogError("Error message {error}", ex.Message);
-            }
-            finally
-            {
-               
-            }
-        }*/
-
-        /*public async Task<List<ServiceNameWithUrl>> ReadServiceNameWithUrlListAsync()
-        {
-            List<ServiceNameWithUrl> uniquensUrls = [];
-            JsonRepository jsonRepository = new(mRepositoryListPath);
-
-            try
-            {
-                uniquensUrls = await jsonRepository.ReadJsonFileAsync<List<ServiceNameWithUrl>>(cUrlWithNameListFileName);
-                return uniquensUrls;
-            }
-            catch (Exception ex)
-            {
-                mLogger.LogError("{error}", ex.Message);
-                return uniquensUrls;
-            }
-        }*/
-
-
-        /*public async Task<List<ServiceInfoModel>> ReadServiceUpdateListsAsync()
-        {
-            List<ServiceInfoModel> serviceInfos = [];
-            JsonRepository jsonRepository = new(mRepositoryListPath);
-            
-            try
-            {
-                List<ServiceUrlModel> uniquensUrls = await jsonRepository.ReadJsonFileAsync<List<ServiceUrlModel>>(cUniqueUrlListFileName);
-
-                foreach (ServiceUrlModel url in uniquensUrls)
-                {
-                    ServiceInfoModel readJson = await jsonRepository.ReadJsonFileAsync<ServiceInfoModel>($"{url.ServiceUrl.ConvertToRepositoryName()}.json");
-                    serviceInfos.Add(readJson);   
-                }
-            }
-            catch (Exception ex)
-            {
-                mLogger.LogError("{error}", ex.Message);
-            }
-        
-            return serviceInfos;
-        }*/
 
         #endregion
 
